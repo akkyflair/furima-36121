aac89110e0a925f1d7cf7486ac5b01d6746367f5
akkyflair <akkyflair@gmail.com>
2021-07-15 11:12:36 +0900

    バリデーション確認とエラーメッセージを出す記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :orders
 
   with_options presence: true do
     validates :nickname
-
-    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
-      validates :family_name
-      validates :first_name
-    end
-
-    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-      validates :family_name_kana
-      validates :first_name_kana
-    end
-
     validates :birthday
   end
+  validates :email, uniqueness: true
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
+  has_many :items
+  has_many :orders
 end
