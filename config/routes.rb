48fa6d4d12e6e0b8fd2a5d277f8407a4cc60859b
akkyflair <akkyflair@gmail.com>
2021-07-16 16:15:07 +0900

    修正1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: 'items#index'
+  
   resources :items, only: [:new, :create] do
-    resources :orders, only: [:index, :create]
+
   end
+
 end
