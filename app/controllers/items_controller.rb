96017d7f6319644b6d658ff15d06b6949de13ec2
akkyflair <akkyflair@gmail.com>
2021-07-17 19:01:14 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,11 @@ class ItemsController < ApplicationController
         redirect_to root_path
     else
         render :new
-    end      
+    end
+
+    def show
+    end
+    
   end
 
   private
