402febf8b38ccffe131bca2047bac8385bfb666d
akkyflair <akkyflair@gmail.com>
2021-07-17 20:46:46 +0900

    商品情報を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index ]
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,12 +20,17 @@ class ItemsController < ApplicationController
 
     def show
     end
-    
+
   end
 
   private
+
   def item_params
     params.require(:item).permit(:title, :text, :price, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :image, ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
