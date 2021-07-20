bb7e74bdbb497964d731b85d95e3fe05ad60be52
akkyflair <akkyflair@gmail.com>
2021-07-20 22:44:30 +0900

    記述追加（売却済み商品編集トップページへ）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_edit, only: [:edit, :update, :destroy]
+  before_action :order_item, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -53,4 +54,11 @@ class ItemsController < ApplicationController
   def move_to_edit
     redirect_to root_path unless current_user.id == @item.user_id
   end
+
+  def order_item
+    if @item.order.present?
+      redirect_to root_path
+    end
+  end
+  
 end
