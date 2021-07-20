df13da4d779edc49edc81e134769d2e35570f563
akkyflair <akkyflair@gmail.com>
2021-07-20 17:05:12 +0900

    トークンの値を非表示

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,11 +13,14 @@ const pay = () => {
       exp_year: `20${formData.get("order_address[exp_year]")}`,
       cvc: formData.get("order_address[cvc]"),
     };
-    
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        
       }
     });
   });
