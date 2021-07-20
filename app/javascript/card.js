a9826e9e8825f51e13a39c411ed4d4a01a9b03fb
akkyflair <akkyflair@gmail.com>
2021-07-20 16:51:38 +0900

    トークン化の処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_57325eb66668aeff7dd2c7fa"); 
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+      cvc: formData.get("order_address[cvc]"),
+    };
+    
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
