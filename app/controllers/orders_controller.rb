d1cafd7e09b5a170e2064cf27a5efc9682a7860c
akkyflair <akkyflair@gmail.com>
2021-07-20 14:06:22 +0900

    sold out 表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create,]
   before_action :set_item, only: [:index, :create]
   before_action :contributor_confirmation, only: [:index, :create]
-
+  before_action :sold_out_item, only: [:index]
   
   def index
     @order_address = OrderAddress.new
@@ -32,5 +32,9 @@ class OrdersController < ApplicationController
       redirect_to root_path 
     end
   end
+
+   def sold_out_item
+    redirect_to root_path if @item.order.present?
+   end
 
 end
